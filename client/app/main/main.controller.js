84e25ba33e9c74766905f346e6c8e6951104771d
vikashvverma <vikash@programminggeek.in>
2015-07-26 16:09:18 +0530

    Update layout :point_up:

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,8 +6,6 @@ angular.module('stickyApp')
     vm.isLoggedIn = Auth.isLoggedIn;
     vm.isAdmin = Auth.isAdmin;
     vm.getCurrentUser = Auth.getCurrentUser;
-    vm.years=[];
-    vm.branches=[];
 
     vm.logout = function () {
       Auth.logout();
@@ -24,18 +22,7 @@ angular.module('stickyApp')
         url: 'main.notes'
       }
     ];
-    vm.extraMenu=[{
-      icon: "fa fa-lock fa-2x",
-      title: "About",
-      tooltip: "About Us",
-      url:'https://github.com/vikashvverma'
-    },{
-      icon: "fa fa-lock fa-2x",
-      title: "Contact us",
-      tooltip: "Contact Us",
-      url:'http://www.google.com/+VikashVerma'
-    }
-    ];
+    vm.extraMenu=[];
 
 
   });
